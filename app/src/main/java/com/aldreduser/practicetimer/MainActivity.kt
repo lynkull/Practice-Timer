14cb74f0cd894370c35af2c165d959b8be37ec51
Ed Cres <crespoed07@gmail.com>
2020-01-21 23:58:29 -0500

    set up classes and functions to save and manipulate timer + more

--- app/src/main/java/com/aldreduser/practicetimer/MainActivity.kt
+++ app/src/main/java/com/aldreduser/practicetimer/MainActivity.kt
@@ -1,23 +1,79 @@
 package com.aldreduser.practicetimer
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.aldreduser.practicetimer.util.PrefUtil
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    enum class TimerState{
+        Stopped, Paused, Running
+    }
+
+    private lateinit var timer:CountDownTimer
+    private var timerLengthSeconds = 0L     //L is for Long
+    private var timerState = TimerState.Stopped
+    private var secondsRemaining = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        supportActionBar?.setIcon(R.drawable.ic_timer)
+        supportActionBar?.title = "    Timer"
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab_start.setOnClickListener{v ->
+            startTimer()
+            timerState = TimerState.Running
+            updateButtons()
+        }
+        fab_pause.setOnClickListener {v ->
+            timer.cancel()
+            timerState = TimerState.Paused
+            updateButtons()
+        }
+        fab_stop.setOnClickListener {v ->
+            timer.cancel()
+            onTimerFinished()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        initTimer()
+
+        //TODO: remove background timer. And hide notification
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if (timerState == TimerState.Running) {
+            timer.cancel()
+            // TODO: start background timer and show notification
+        } else if(timerState == TimerState.Paused){
+            //TODO: show notification
+        }
+
+        PrefUtil.setPreviousTimerLengthSeconds(timerLengthSeconds, this)
+        PrefUtil.setSecondsRemaining(secondsRemaining, this)
+        PrefUtil.setTimerState(timerState, this)
+    }
+
+    private fun initTimer(){
+        timerState = PrefUtil.getTimerState(this)
+
+        if (timerState == TimerState.Stopped){
+            setNewTimerLength()
+        } else {
+            setPreviousTimerLength()
         }
     }
 
---
+++ app/src/main/java/com/aldreduser/practicetimer/util/PrefUtil.kt
@@ -0,0 +1,53 @@
+package com.aldreduser.practicetimer.util
+
+import android.content.Context
+import android.preference.PreferenceManager
+import com.aldreduser.practicetimer.MainActivity
+import java.util.*
+
+class PrefUtil {
+    companion object {
+
+        // need a context to be able to extract the value from preferneces
+        fun getTimerLength(context: Context): Int{
+            //placeholder
+            //minutes
+            return 1
+        }
+
+        private const val PREVIOUS_TIMER_LENGTH_SECONDS_ID = "com.aldreduser.timer.previous_timer_length"
+        fun getPreviousTimerLengthSeconds(context: Context): Long{
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, 0)
+        }
+        fun setPreviousTimerLengthSeconds(seconds: Long, context: Context) {
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            editor.putLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, seconds)
+            editor.apply()
+        }
+
+        private const val TIMER_STATE_ID = "com.aldreduser.timer.timer_state"
+        fun getTimerState(context: Context): MainActivity.TimerState{
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            val ordinal = preferences.getInt(TIMER_STATE_ID, 0)
+            return MainActivity.TimerState.values()[ordinal]
+        }
+        fun setTimerState(state: MainActivity.TimerState, context: Context){
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            val ordinal = state.ordinal
+            editor.putInt(TIMER_STATE_ID, ordinal)
+            editor.apply()
+        }
+
+        private const val SECONDS_REMAINING_ID = "com.aldreduser.timer.seconds_remaining"
+        fun getSecondsRemaining(context: Context): Long{
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getLong(SECONDS_REMAINING_ID, 0)
+        }
+        fun setSecondsRemaining(seconds: Long, context: Context) {
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            editor.putLong(SECONDS_REMAINING_ID, seconds)
+            editor.apply()
+        }
+    }
+}
